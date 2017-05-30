5adc72bba3540608945ae3d749d644971482cbc9
menapro <medina@menapro.com>
2017-05-30 17:42:33 +0200

    Update Contactform.php

--- blocks/contactform/frontend/contactformmodule/Contactform.php
+++ blocks/contactform/frontend/contactformmodule/Contactform.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: silvia
- * Date: 21/11/2016
- * Time: 11:32
- */
+
 namespace blocks\contactform\frontend\contactformmodule;
 
 class Contactform extends \yii\base\Module
